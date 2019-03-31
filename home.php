c138c85c36cf93058cdb5505bdc1579c6199761c
sagerdmn <30279650+sagerdmn@users.noreply.github.com>
2019-03-31 09:55:38 -0500

    View open tickets test

--- home.php
+++ home.php
@@ -59,8 +59,79 @@ $tickets = $row[0];
 		<div id="openTicket"><button type="button" class="button" onClick="location.href='OpenTicket.php'">Open Ticket</button></div>
 		<div id="viewTickets"><button type="button" class="button" onClick="location.href='ViewTicket.php'">View Tickets</button></div>
 	</span>
+
 	<br><br>
 	
+	<?php
+		
+			//INCOMING POST DATA
+			$techUN=$_POST['techUN'];
+			$status=$_POST['status'];
+			
+			//USED FOR TESTING
+			/*
+			$techUN="jknutson";
+			$status="Active";
+			*/
+			
+			//SERVER CONNECTION CREDENTIALS
+			$servername = "joelknutson.net";
+			$username = "joelknut_csc450";
+			$pw = "CSP@2019";
+			$dbName = "joelknut_csc450";
+
+			//BUILD CONNECTION STRING
+			$conn = new mysqli($servername, $username, $pw, $dbName);
+
+			//TRY CONNECTION
+			if($conn->connect_error){
+
+				die("Connection failed: ".$dbConn->connect_error);
+			}
+			
+			//BUILD QUERY STRING
+			$sql = "SELECT * FROM TICKET WHERE TECH_UN='".$techUN."' AND STATUS='".$status."'";
+
+			//ASSIGN DATA TO ARRAY
+			$result=$conn->query($sql);			
+			
+			//PARSE ARRAY AND DISPLAY DATA
+			//
+			echo "Tickets";
+			echo "<br/>";
+			echo "-------------------------------";
+			echo "<br/>";
+			
+			if ($result->num_rows > 0){
+					
+				while($row = $result->fetch_assoc()){
+
+					echo "<a href=\"return-ticket-notes.php?ticketID=".$row["TICKET_ID"]."\">";
+					echo "Ticket ID:  ";
+					echo $row["TICKET_ID"];
+					echo "<br/>";
+					echo "Title:  ";
+					echo $row["TICKET_TITLE"];
+					echo "<br/>";
+					echo "Status:  ";
+					echo $row["STATUS"];
+					echo "<br/>";
+					echo "End User:  ";
+					echo $row["USER_UN"];
+					echo "<br/>";
+					echo "-------------------------------";
+					echo "</a>";
+					echo "<br/>";
+					
+				}
+
+			} else{
+				echo "** No Tickets Found **".$conn->error;
+			}
+
+			$conn->close();
+
+	?>
 	
 	<!-- jQuery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
