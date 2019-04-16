e6e4cbf49f46b342388b4190ceeb1f518aab3fe2
sagerdmn <30279650+sagerdmn@users.noreply.github.com>
2019-04-15 21:40:08 -0500

    Created header.php to clean up code

--- OpenTicket.php
+++ OpenTicket.php
@@ -1,77 +1,8 @@
 ﻿<?php
-session_start();
-if($_SESSION['status'] != "Active") {
-	header("Location: index.html");
-}	
-$servername = "joelknutson.net";
-$username = "joelknut_csc450";
-$pw = "CSP@2019";
-$dbName = "joelknut_csc450";
-$conn = new mysqli($servername, $username, $pw, $dbName);
-//Total Tickets
-$sql = "SELECT COUNT(*) FROM TICKET WHERE USER_UN = '".$_SESSION['currentUser']."' OR TECH_UN = '".$_SESSION['currentUser']."'";
-$result=$conn->query($sql);
-$row = mysqli_fetch_array($result);
-$totalTickets = $row[0];
-//User created tickets
-$sqlc = "SELECT COUNT(*) FROM TICKET WHERE USER_UN = '".$_SESSION['currentUser']."'";
-$resultc=$conn->query($sqlc);
-$rowc = mysqli_fetch_array($resultc);
-$createdTickets = $rowc[0];
-//Assigned Tickets
-$sqla = "SELECT COUNT(*) FROM TICKET WHERE TECH_UN = '".$_SESSION['currentUser']."'";
-$resulta=$conn->query($sqla);
-$rowa = mysqli_fetch_array($resulta);
-$assignedTickets = $rowa[0];
-
-//GET TICKETS THAT CURRENT USER IS INVOLVED WITH
-$sqlUN = "SELECT * FROM TICKET WHERE USER_UN = '".$_SESSION['currentUser']."' OR TECH_UN='".$_SESSION['currentUser']."'";
-$resultUN=$conn->query($sqlUN);
-//String for holding ticket SQL query text
-$IdString = '';
-//build string
-if($resultUN->num_rows > 0){
-	//while loop formats table data
-	while($rowUN = $resultUN->fetch_assoc()){
-		$IdString = $IdString . 'TICKET_ID=' . $rowUN['TICKET_ID'] . ' OR ';
-	}
-}
-//remove last OR
-$IdString = substr($IdString, 0, -4);
-//get count of unread tickets from $IdString
-$sqlUnCt = "SELECT COUNT(*) FROM NOTE WHERE (".$IdString.") AND OWNER_UN!='".$_SESSION['currentUser']."' AND UNREAD=1";
-$resultUnCt=$conn->query($sqlUnCt);
-$rowUnCt = mysqli_fetch_array($resultUnCt);
-$Unread = $rowUnCt[0];
-
-/////IF NOTE HAS BEEN VIEWED FEATURE////////
-//The user loading the page is either the creator or the assignee of the selected ticket.
-if($_SESSION['currentUser'] === $data['USER_UN'] || $data['TECH_UN']){
-	//If the current user is the assignee of the ticket.
-	if($_SESSION['currentUser'] === $data['TECH_UN']){
-		//If the Creator user did create the most recent comment
-		if($_SESSION['currentUser'] === $data1['OWNER_UN'] ){
-			//echo "</br>";
-			//echo "Unread is not changed, you made the last comment.";
-		}
-		//If the Creator user did NOT create the most recent comment
-		if($_SESSION['currentUser'] !== $data1['OWNER_UN'] ){
-			//echo "</br>";
-			//echo "Unread is being changed, you did not make the last comment.";
-			//Make change on ticket UNREAD VALUE
-			$sql5 = "UPDATE NOTE SET UNREAD_TECH = 0 WHERE TICKET_ID = '".$_POST['selectedID']."' AND NOTE_ID = '".$data1['NOTE_ID']."' ";
-			$result5=$conn->query($sql5);
-			$sql6 = "SELECT * FROM NOTE WHERE TICKET_ID= '".$_POST['selectedID']."' order by NOTE_ID desc limit 1";
-			$result6=$conn->query($sql6);
-			if (mysqli_num_rows($result6) > 0) {
-				$data2 = mysqli_fetch_array($result6);
-			}
-		}
-	}
-}
-
-
+//add header
+include('header.php');
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
--- home.php
+++ home.php
@@ -1,49 +1,6 @@
 ﻿<?php
-session_start();
-if($_SESSION['status'] != "Active") {
-	header("Location: index.html");
-}	
-$servername = "joelknutson.net";
-$username = "joelknut_csc450";
-$pw = "CSP@2019";
-$dbName = "joelknut_csc450";
-$conn = new mysqli($servername, $username, $pw, $dbName);
-//Total Tickets
-$sql = "SELECT COUNT(*) FROM TICKET WHERE USER_UN = '".$_SESSION['currentUser']."' OR TECH_UN = '".$_SESSION['currentUser']."'";
-$result=$conn->query($sql);
-$row = mysqli_fetch_array($result);
-$totalTickets = $row[0];
-//User created tickets
-$sqlc = "SELECT COUNT(*) FROM TICKET WHERE USER_UN = '".$_SESSION['currentUser']."'";
-$resultc=$conn->query($sqlc);
-$rowc = mysqli_fetch_array($resultc);
-$createdTickets = $rowc[0];
-//Assigned Tickets
-$sqla = "SELECT COUNT(*) FROM TICKET WHERE TECH_UN = '".$_SESSION['currentUser']."'";
-$resulta=$conn->query($sqla);
-$rowa = mysqli_fetch_array($resulta);
-$assignedTickets = $rowa[0];
-
-//GET TICKETS THAT CURRENT USER IS INVOLVED WITH
-$sqlUN = "SELECT * FROM TICKET WHERE USER_UN = '".$_SESSION['currentUser']."' OR TECH_UN='".$_SESSION['currentUser']."'";
-$resultUN=$conn->query($sqlUN);
-//String for holding ticket SQL query text
-$IdString = '';
-//build string
-if($resultUN->num_rows > 0){
-	//while loop formats table data
-	while($rowUN = $resultUN->fetch_assoc()){
-		$IdString = $IdString . 'TICKET_ID=' . $rowUN['TICKET_ID'] . ' OR ';
-	}
-}
-//remove last OR
-$IdString = substr($IdString, 0, -4);
-//get count of unread tickets from $IdString
-$sqlUnCt = "SELECT COUNT(*) FROM NOTE WHERE (".$IdString.") AND OWNER_UN!='".$_SESSION['currentUser']."' AND UNREAD=1";
-$resultUnCt=$conn->query($sqlUnCt);
-$rowUnCt = mysqli_fetch_array($resultUnCt);
-$Unread = $rowUnCt[0];
-
+//add header
+include('header.php');
 ?>
 <!doctype html>
 
